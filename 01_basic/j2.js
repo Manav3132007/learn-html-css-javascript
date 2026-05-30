32e2b530bb3c8a55bbcc643ab39fe810732ea5ef
Manav3132007 <manavsathvara0@gmail.com>
2026-05-30 09:46:51 +0000

    varible

---
+++ 01_basic/j2.js
@@ -0,0 +1,11 @@
+const accountId=14354233
+var accounEmail="manavsathvara@gamil.com";
+let accountnumber= "1088544534";
+accountciy= "Ahamadabad";
+
+
+ accounEmail="parth@gamil.com";
+ accountnumber="32455555";
+accountciy= "jaupura";
+
+console.table([accounEmail,accountnumber,accountciy]);
---
+++ 01_basic/j3.js
@@ -0,0 +1,18 @@
+let a="manav";   // "" string
+let b=12;  //number 2 to power 53
+let c=true; // boolen 
+let d=900; // bigint
+let n= undefined; // undefined
+let f; // null
+console.log(typeof a);
+
+
+//treqt qll js code as newer version
+
+// alert(3+3)// we are using nodejs, not browser
+
+console.log(3
+    +
+3); //code readability should be high
+
+
