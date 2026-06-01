ad49382229a0654e2faf655aa9191f6445a4f727
Manav3132007 <manavsathvara0@gmail.com>
2026-06-01 10:56:59 +0000

    datatypr summrey

--- 01_basic/j4.js
+++ 01_basic/j4.js
@@ -29,3 +29,5 @@ let stringNumber =String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+let value = 3;
+let negvalue = -value;
---
+++ 01_basic/j5.js
@@ -0,0 +1,75 @@
+
+//*******************************convertion******************************************* */
+
+console.log(negvalue);
+ // +,-,,*,/,%
+
+ //console.log("1"+2);
+ //console.log(1+"2");
+ //console.log("1"+2+2);
+ //console.log(1+2+"2");
+
+ //console.log((3+4) *5 % 3);
+ 
+
+ //console.log(+true);
+ //console.log(+"");
+
+
+let gamecounter=100;
+
+++gamecounter;
+
+console.log(gamecounter);
+
+
+//cpmperition oprition
+
+//>,>=,<,,<=,==,!=
+
+// datatype ma value lakhi huya tu same number hyut tu compera value na thya  value false ja ave
+console.log("2" > 1);
+console.log("02" > 1);
+
+//confution
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+
+//=== comper ni thya not comaper
+
+console.log("2" == 2);
+console.log("2"===2);
+
+//**********************datatype -summary***************************************** */
+
+// primitive datatype
+
+/*
+  7 type : String,number,boolearn,null,undefind,symbol,bigint
+*/
+
+const score = 100
+const scorevalue = 100.3
+
+const isloggedin = false
+const outideTemp = null
+let userEmail;
+
+const id= Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
+
+
+// Reference (non primitive datatype)
+
+/*
+   Array,objects, functions
+*/
+
