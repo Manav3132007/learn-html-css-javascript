210b25dfe95d4d601a1a27e66c76a52abef7f3f1
Manav3132007 <manavsathvara0@gmail.com>
2026-06-06 10:02:18 +0000

    String mathod

---
+++ 01_basic/j7.js
@@ -0,0 +1,39 @@
+const name="chamunda maa"
+const repo="churu manav"
+
+console.log(`tara bada ne hachavi rakhaje ${name} taru ${repo} ni laja rakhva avaje maa `);
+
+const gameName = new String('manav-hc-com')
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+
+//console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf('n'));
+
+const newm= gameName.substring(0,4);
+console.log(newm);
+
+const another=gameName.slice(-4,2);
+console.log(another);
+
+const newstring = "  manav  ";
+console.log(newstring);
+console.log(newstring.trim());
+
+const url="https://manav.com/manav%20sathvara"
+
+console.log(url.replace('%20','_'));
+
+console.log(url.includes('manav'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
